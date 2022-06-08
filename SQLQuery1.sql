d0380d64dc75b19d6fcedd79582af27fe099546a
luchintrinh <chintrinhhk15clc123@gmail.com>
2022-06-08 15:08:06 +0700

    trinh sua lai

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -38,9 +38,9 @@ go
 create table diem
 (
 
-	mahs char(15) references hocsinh(mahs),
-	malop char(15) references lop(malop),
-	ten_mon_hoc nvarchar(15) references mon_hoc(ten_mon_hoc),
+	mahs char(10) references hocsinh(mahs),
+	malop char(10) references lop(malop),
+	mamh char(10) references mon_hoc(mamh),
 --ki1
 	diem_mieng_ki_1 float,
 	diem_15_ki_1 float,
@@ -69,6 +69,7 @@ go
 --tao bảng môn học 
 create table mon_hoc
 (
+	mamh char(10) primary key,
 	ten_mon_hoc nvarchar(15),
 	magv char(10) references giaovien(magv)
 
@@ -76,7 +77,9 @@ create table mon_hoc
 go
 
 
-  
+
+
+  select *from diem
 
 --nhập bảng lớp 
 insert LOP values
@@ -93,13 +96,19 @@ insert giaovien values
 --nhập bảng học sinh
 insert hocsinh values
 ('00001', 'NGUYEN THI DIU','0987654321','NU','2022_10A'),
-('00002', '','','','');
+('00002', '','','','2022_10A');
+--nhập bảng môn học
+
+insert mon_hoc values
+('TA', N'Tiếng Anh', '0001'),
+('GDCD', N'Giáo dục', '0002');
 
 --nhập bảng điểm
+
 insert diem values
-('00001', '2022_10A','TOAN','1','2','3','4','5','tot','gioi','1','2','3','4','5','tot','gioi','diem_tb_canam'),
-('00002',  '2022_10A','TOAN','1','2','3','4','5','tot','gioi','1','2','3','4','5','tot','gioi','diem_tb_canam' );
+('00001', '2022_10A','TA','1','2','3','4','5','tot','gioi','1','2','3','4','5','tot','gioi', 9),
+('00002',  '2022_10A','GDCD','1','2','3','4','5','tot','gioi','1','2','3','4','5','tot','gioi',9 );
 
 
-
+select * from diem
 
